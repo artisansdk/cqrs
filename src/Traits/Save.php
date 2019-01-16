f9bf3ea0fdddd4cdf0bbdea5375da0d0e75393ec
Daniel LaBarge <daniel@bexarcreative.com>
2019-01-16 13:16:09 -0600

    Improve debug output.

--- src/Traits/Save.php
+++ src/Traits/Save.php
@@ -21,10 +21,13 @@ trait Save
             if ('cli' === php_sapi_name() || 'phpdbg' === php_sapi_name()) {
                 $errors = implode(PHP_EOL.'- ', (array) $model->getErrors()->all());
                 $message = sprintf(
-                    'The %s model could not be saved because the attributes were invalid:%s%s',
+                    'The %s model could not be saved because the attributes were invalid:%s%s%sThe attributes for the model are:%s%s',
                     get_class($model),
                     PHP_EOL,
-                    $errors
+                    $errors,
+                    PHP_EOL,
+                    PHP_EOL,
+                    $model->toJson(JSON_PRETTY_PRINT)
                 );
             }
 
